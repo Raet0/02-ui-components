995a120b9d33d502d1330267ab0aac323e209873
Raet0 <Yosoyrafaelprieto@gmail.com>
2025-11-22 23:23:23 -0500

    modificación redme con link de pagina desplegada

---
+++ chunk-EDVKE3HW.js
@@ -0,0 +1,2 @@
+import{Ha as n,Za as i,_a as o,fb as t}from"./chunk-2URJ2TWP.js";var p=class a{static \u0275fac=function(e){return new(e||a)};static \u0275cmp=n({type:a,selectors:[["app-h8-page"]],decls:3,vars:0,template:function(e,c){e&1&&(i(0,"p"),t(1,"h8-page works!"),o(),t(2,`,
+`))},encapsulation:2,changeDetection:0})};export{p as H8Page};
