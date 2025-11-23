981f1ec70bd00ff0a87d2d798dc4985fb767453c
Raet0 <Yosoyrafaelprieto@gmail.com>
2025-11-22 23:21:12 -0500

    Auto-generated commit

---
+++ chunk-FLBMQON7.js
@@ -0,0 +1,2 @@
+import{Ha as n,Za as i,_a as o,fb as t}from"./chunk-4PHBZPLT.js";var p=class a{static \u0275fac=function(e){return new(e||a)};static \u0275cmp=n({type:a,selectors:[["app-h7-page"]],decls:3,vars:0,template:function(e,c){e&1&&(i(0,"p"),t(1,"h7-page works!"),o(),t(2,`,
+`))},encapsulation:2,changeDetection:0})};export{p as H7Page};
